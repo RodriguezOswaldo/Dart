868158ff0dc3eaa6979a494f2323e2851b0fdeca
rodriguezoswaldo <owner2303@gmail.com>
2020-08-17 12:16:15 -0400

    abstract classes, implement keyword

---
+++ 6-Constructors/abstract.dart
@@ -0,0 +1,26 @@
+void main(){
+  final perro = new Perro();
+  perro.emitirSonido();
+  
+  final gato = new Gato();
+  gato.emitirSonido();
+// 
+}
+
+abstract class Animal{
+  int patas;
+  void emitirSonido(){
+    
+  }
+  
+}
+class Perro implements Animal{
+   int patas;
+   int colas;
+  void emitirSonido()=> print('Guauuuu!!');
+}
+
+class Gato implements Animal{
+   int patas;
+  void emitirSonido()=> print('Miawwwww!!');
+}
