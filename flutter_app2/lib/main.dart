5a2f5238764058e77d250b7356d36b2233b1c4c8
Oswaldo Rodriguez <OwnOS@oswaldos-mbp.home>
2020-08-18 11:00:42 -0400

    from Android

--- flutter_app2/lib/main.dart
+++ flutter_app2/lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hola Mundo!>>!!:',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
