ed7cb22b50a40223a703421eef539458890725c0
Oswaldo Rodriguez <OwnOS@oswaldos-mbp.home>
2020-08-18 13:58:36 -0400

    fixing stuff

--- firstFlutter/flutter_app/lib/main.dart
+++ firstFlutter/flutter_app/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Counting App!'),
     );
   }
 }
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hola Mundo!>>!!:',
+              'Number:',
             ),
             Text(
               '$_counter',
